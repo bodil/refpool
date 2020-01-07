22152a7d220d71e7c201bd5cebe1c5cf33dcbde1
Bodil Stokke <bodil@bodil.org>
2020-01-07 22:43:14 +0000

    Criterion benchmarks.

--- benches/pool.rs
+++ benches/pool.rs
@@ -2,107 +2,109 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#![feature(test)]
-
-extern crate test;
-
-use refpool::{Pool, PoolRef, PoolSyncType, PoolUnsync};
 use std::rc::Rc;
-use test::Bencher;
-
-#[cfg(feature = "sync")]
 use std::sync::Arc;
 
-#[cfg(feature = "sync")]
-use refpool::PoolSync;
+use criterion::{
+    black_box, criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput,
+};
 
-type TestType = usize;
+use refpool::{Pool, PoolDefault, PoolRef, PoolSync, PoolSyncType, PoolUnsync};
 
-fn alloc_131072_with_pool<S: PoolSyncType<TestType>>(b: &mut Bencher) {
-    let pool = Pool::<TestType, S>::new(131_072);
-    pool.fill();
-    let mut vec: Vec<_> = Vec::with_capacity(131_072);
-    b.iter(|| {
-        for _ in 0..131_072 {
-            vec.push(PoolRef::default(&pool));
-        }
-        vec.clear();
-    })
-}
+const SIZES: &[usize] = &[1024, 2048, 4096, 8192, 16384, 32768, 65536, 131_072];
 
-fn alloc_131072_without_pool<Ref: Default>(b: &mut Bencher) {
-    let mut vec: Vec<Ref> = Vec::with_capacity(131_072);
-    b.iter(|| {
-        for _ in 0..131_072 {
-            vec.push(Default::default());
-        }
-        vec.clear();
-    })
-}
-
-fn alloc_dealloc_131072_with_pool<S: PoolSyncType<TestType>>(b: &mut Bencher) {
-    let pool = Pool::<TestType, S>::new(131_072);
-    {
-        let chunk = PoolRef::default(&pool);
-        test::black_box(chunk);
+pub fn alloc<A: PoolDefault, S: PoolSyncType<A>, P: Default>(name: &str, c: &mut Criterion) {
+    let mut group = c.benchmark_group(name);
+    for size in SIZES {
+        group.throughput(Throughput::Elements(*size as u64));
+        group.bench_with_input(BenchmarkId::new("sysalloc", size), size, |b, &size| {
+            b.iter_batched_ref(
+                || Vec::with_capacity(size),
+                |vec| {
+                    for _ in 0..size {
+                        vec.push(P::default());
+                    }
+                },
+                BatchSize::SmallInput,
+            )
+        });
+        group.bench_with_input(BenchmarkId::new("empty pool", size), size, |b, &size| {
+            b.iter_batched_ref(
+                || (Pool::<A, S>::new(size), Vec::with_capacity(size)),
+                |&mut (ref pool, ref mut vec)| {
+                    for _ in 0..size {
+                        vec.push(PoolRef::default(pool));
+                    }
+                },
+                BatchSize::SmallInput,
+            );
+        });
+        group.bench_with_input(BenchmarkId::new("full pool", size), size, |b, &size| {
+            b.iter_batched_ref(
+                || {
+                    let pool = Pool::<A, S>::new(size);
+                    pool.fill();
+                    (pool, Vec::with_capacity(size))
+                },
+                |&mut (ref pool, ref mut vec)| {
+                    for _ in 0..size {
+                        vec.push(PoolRef::default(pool));
+                    }
+                },
+                BatchSize::SmallInput,
+            );
+        });
     }
-    b.iter(|| {
-        for _ in 0..131_072 {
-            let chunk = PoolRef::default(&pool);
-            test::black_box(chunk);
-        }
-    })
+    group.finish();
 }
 
-fn alloc_dealloc_131072_without_pool<Ref: Default>(b: &mut Bencher) {
-    b.iter(|| {
-        for _ in 0..131_072 {
-            let chunk: Ref = Default::default();
-            test::black_box(chunk);
-        }
-    })
+pub fn realloc<A: PoolDefault, S: PoolSyncType<A>, P: Default>(name: &str, c: &mut Criterion) {
+    let mut group = c.benchmark_group(name);
+    for size in SIZES {
+        group.throughput(Throughput::Elements(*size as u64));
+        group.bench_with_input(BenchmarkId::new("sysalloc", size), size, |b, &size| {
+            b.iter(|| {
+                for _ in 0..size {
+                    black_box(P::default());
+                }
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("pool", size), size, |b, &size| {
+            b.iter_batched_ref(
+                || Pool::<A, S>::new(size),
+                |pool| {
+                    for _ in 0..size {
+                        black_box(PoolRef::default(pool));
+                    }
+                },
+                BatchSize::SmallInput,
+            );
+        });
+    }
+    group.finish();
 }
 
-#[bench]
-fn alloc_131072_with_unsync_pool(b: &mut Bencher) {
-    alloc_131072_with_pool::<PoolUnsync>(b)
+fn alloc_unsync_usize(c: &mut Criterion) {
+    alloc::<usize, PoolUnsync, Rc<usize>>("alloc/unsync/usize", c)
 }
 
-#[bench]
-fn alloc_131072_without_unsync_pool(b: &mut Bencher) {
-    alloc_131072_without_pool::<Rc<TestType>>(b)
+fn alloc_sync_usize(c: &mut Criterion) {
+    alloc::<usize, PoolSync, Arc<usize>>("alloc/sync/usize", c)
 }
 
-#[bench]
-fn alloc_dealloc_131072_with_unsync_pool(b: &mut Bencher) {
-    alloc_dealloc_131072_with_pool::<PoolUnsync>(b)
+fn realloc_unsync_usize(c: &mut Criterion) {
+    realloc::<usize, PoolUnsync, Rc<usize>>("realloc/unsync/usize", c)
 }
 
-#[bench]
-fn alloc_dealloc_131072_without_unsync_pool(b: &mut Bencher) {
-    alloc_dealloc_131072_without_pool::<Rc<TestType>>(b)
+fn realloc_sync_usize(c: &mut Criterion) {
+    realloc::<usize, PoolSync, Arc<usize>>("realloc/sync/usize", c)
 }
 
-#[cfg(feature = "sync")]
-#[bench]
-fn alloc_131072_with_sync_pool(b: &mut Bencher) {
-    alloc_131072_with_pool::<PoolSync>(b)
-}
-
-#[cfg(feature = "sync")]
-#[bench]
-fn alloc_131072_without_sync_pool(b: &mut Bencher) {
-    alloc_131072_without_pool::<Arc<TestType>>(b)
-}
-
-#[cfg(feature = "sync")]
-#[bench]
-fn alloc_dealloc_131072_with_sync_pool(b: &mut Bencher) {
-    alloc_dealloc_131072_with_pool::<PoolSync>(b)
-}
-
-#[cfg(feature = "sync")]
-#[bench]
-fn alloc_dealloc_131072_without_sync_pool(b: &mut Bencher) {
-    alloc_dealloc_131072_without_pool::<Arc<TestType>>(b)
-}
+criterion_group!(
+    refpool,
+    alloc_unsync_usize,
+    alloc_sync_usize,
+    realloc_unsync_usize,
+    realloc_sync_usize
+);
+criterion_main!(refpool);
